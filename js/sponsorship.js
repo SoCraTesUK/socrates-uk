649f1a5eeb8dfa58011a37a5d9636b63210e921b
Gonçalo Silva <gnorsilva@gmail.com>
2013-08-21 19:07:02 +0100

    improving the sponsors randomize algorithm

--- js/sponsorship.js
+++ js/sponsorship.js
@@ -1,10 +1,8 @@
 $(function() {
-    var day = new Date().getTime() / (24 * 60 * 60 * 1000) | 0,
-        rng = new MersenneTwister(day),
-        sponsors = $('#sponsors'),
+    var sponsors = $('#sponsors'),
         sortedSponsorElements;
 
-    sortedSponsorElements = $('.sponsor', sponsors).get().sort(function() { return rng.random() - 0.5; });
+    sortedSponsorElements = $('.sponsor', sponsors).get().sort(function() { return (Math.round(Math.random())-0.5); });
 
     $.each(sortedSponsorElements, function(_, element) {
         sponsors.append($(element));
